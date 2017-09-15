43448e508baff98c8573f7382dc315307803e703
Timothy Williams <tim@terabytetim.com>
2017-09-15 18:57:39 -0400

    - Added all methods to EnemySpriteFactory

--- Lasagna/Utilities/EnemySpriteFactory.cs
+++ Lasagna/Utilities/EnemySpriteFactory.cs
@@ -3,8 +3,12 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Lasagna
 {
-    class EnemySpriteFactory
+    public class EnemySpriteFactory
     {
+        private Texture2D goombaDieSheet;
+        private Texture2D goombaWalkSheet;
+        private Texture2D koopaDieSheet;
+        private Texture2D koopaWalkSheet;
 
         private static EnemySpriteFactory instance;
 
@@ -21,6 +25,29 @@ namespace Lasagna
 
         public void LoadAllContent(ContentManager content)
         {
+            goombaDieSheet = content.Load<Texture2D>("EnemySprites/Goomba_Die");
+            goombaWalkSheet = content.Load<Texture2D>("EnemySprites/Goomba_Walk");
+            koopaDieSheet = content.Load<Texture2D>("EnemySprites/Koopa_Die");
+            koopaWalkSheet = content.Load<Texture2D>("EnemySprites/Koopaa_Walk");
+        }
+
+        public NonAnimatedSprite CreateSprite_Goomba_Die()
+        {
+            return new NonAnimatedSprite(goombaDieSheet, 16, 8);
+        }
+
+        public AnimatedSprite CreateSprite_Goomba_Walk()
+        {
+            return new AnimatedSprite(goombaWalkSheet, 2, 1, 16, 16, 24);
+        }
+        public AnimatedSprite CreateSprite_Koopa_Die()
+        {
+            return new AnimatedSprite(koopaDieSheet, 2, 1, 16, 15, 24);
+        }
+
+        public AnimatedSprite CreateSprite_Koopa_Walk()
+        {
+            return new AnimatedSprite(koopaWalkSheet, 2, 1, 16, 24, 24);
         }
     }
 }
