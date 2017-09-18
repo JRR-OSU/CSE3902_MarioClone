fc8a8ad02de7ebd52509bc3c749c9da429f3549e
Zeyu Li <li.6778@osu.edu>
2017-09-18 00:53:04 -0400

    Added interfaces for tiles, projectiles, and concrete classes for fireballs and missles.

---
+++ Lasagna/FireProjectile.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+using Lasagna.Interfaces;
+
+namespace Lasagna
+{
+    class FireProjectile : IProjectile
+    {
+        /// <summary>
+        /// State = 0 : Not exploded, State = 1 : Exploded
+        /// </summary>
+        private int State = 0;
+        public void ChangeState()
+        {
+            this.State = 1;
+        }
+        public void Update(GameTime gameTime, SpriteBatch spriteBatch, int spriteXPos, int spriteYPos)
+        {
+            if (this.State == 0)
+            {
+                ISprite fireballDefault = ProjectileSpriteFactory.Instance.CreateSprite_Fireball_Default();
+                fireballDefault.Update(gameTime, spriteXPos, spriteYPos);
+                fireballDefault.Draw(spriteBatch);
+            }
+            else if (this.State == 1)
+            {
+                ISprite fireballExplode = ProjectileSpriteFactory.Instance.CreateSprite_Fireball_Explode();
+                fireballExplode.Update(gameTime, spriteXPos, spriteYPos);
+                fireballExplode.Draw(spriteBatch);
+            }
+        }
+    }
+}
---
+++ Lasagna/MissleProjectile.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+using Lasagna.Interfaces;
+
+namespace Lasagna
+{
+    class MissleProjectile : IProjectile
+    {
+        public void ChangeState()
+        {
+            return;
+        }
+        public void Update(GameTime gameTime, SpriteBatch spriteBatch, int spriteXPos, int spriteYPos)
+        {
+            //Reserved for missle sprite.
+        }
+    }
+}
