efda6a5653d85ab4f78684254d54867de4b5aefd
Zeyu Li <li.6778@osu.edu>
2017-11-07 18:33:53 -0500

    - Updated SFX factory.

--- Lasagna/Utilities/SoundEffectFactory.cs
+++ Lasagna/Utilities/SoundEffectFactory.cs
@@ -5,5 +5,28 @@ namespace Lasagna.Utilities
 {
     public class SoundEffectFactory
     {
+        private SoundEffect oneUp;
+        private SoundEffect bowserFall;
+        private SoundEffect bowserFire;
+        private SoundEffect breakBlock;
+        private SoundEffect bump;
+        private SoundEffect coin;
+        private SoundEffect fireball;
+        private SoundEffect fireworks;
+        private SoundEffect flagpole;
+        private SoundEffect gameover;
+        private SoundEffect jumpMarioBig;
+        private SoundEffect jumpMarioSmall;
+        private SoundEffect kick;
+        private SoundEffect marioDie;
+        private SoundEffect pause;
+        private SoundEffect pipe;
+        private SoundEffect powerUp;
+        private SoundEffect powerUpAppears;
+        private SoundEffect stageClear;
+        private SoundEffect stomp;
+        private SoundEffect vine;
+        private SoundEffect warning;
+        private SoundEffect worldClear;
     }
 }
